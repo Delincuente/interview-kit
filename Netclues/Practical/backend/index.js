592ba9775e3badc06a26a431b7984dac2b399a6e
CRIMINAL <butani.hardik53@gmail.com>
2025-09-29 11:44:15 +0530

    Netcluse: practical: log middleware

--- Netclues/Practical/backend/index.js
+++ Netclues/Practical/backend/index.js
@@ -3,6 +3,10 @@ import express from "express";
 import errorHandler, { notFound } from "./middlewares/errorHandler.js";
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(new Date(), req.method, req.url);
+});
 
 app.use(notFound);
 app.use(errorHandler);
