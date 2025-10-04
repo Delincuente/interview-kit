048bea761eba69fdafa48d315bcd526a496e8051
CRIMINAL <butani.hardik53@gmail.com>
2025-10-04 20:36:40 +0530

    Practical: event methods

---
+++ Netclues/Practical/backend/controllers/eventController.js
@@ -0,0 +1,5 @@
+export const getEvents = (req, res, next) => { }
+export const addEvent = (req, res, next) => { }
+export const editEvent = (req, res, next) => { }
+export const getEvent = (req, res, next) => { }
+export const deleteEvent = (req, res, next) => { }
