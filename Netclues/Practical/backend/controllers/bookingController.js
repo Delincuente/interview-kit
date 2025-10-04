4b043a97ce0d0f812880b147eb1cbb8d723e96bc
CRIMINAL <butani.hardik53@gmail.com>
2025-10-04 20:39:48 +0530

    Practical: booking methods

---
+++ Netclues/Practical/backend/controllers/bookingController.js
@@ -0,0 +1,5 @@
+export const getBookings = (req, res, next) => { }
+export const addBooking = (req, res, next) => { }
+export const getBooking = (req, res, next) => { }
+export const editBooking = (req, res, next) => { }
+export const deleteBooking = (req, res, next) => { }
